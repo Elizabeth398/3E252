bbd85073f293a9089428cea50691d6b5f4193a56
Elizabeth398 <a24310398@live.ceti.mx>
2025-11-03 05:55:21 +0000

    Examen Segundo Parcial

--- src/animacion.cpp
+++ src/animacion.cpp
@@ -1,11 +1,10 @@
-    #include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/color.hpp>
 #include <thread>
 #include <chrono>
 #include <iostream>
 #include <Dibujo.hpp>
 #include <GestorDibujos.hpp>
-#include <Corral.hpp>
 using namespace ftxui;
 
 int main()
@@ -16,63 +15,63 @@ int main()
         Dimension::Fixed(24));
 
     // Agregar código de los elementos de la granja
-    const std::vector<std::u32string> granja1 = {
-        U"  _||_ ",
-        U" |    |",
-        U" |____|",
-        U" |    |",
-        U"/-|__|-\\"};
-    const std::vector<std::u32string> vaca1 = {
-        U"        (__) ",
-        U"` ------(oo) ",
-        U"  ||    (__) ",
-        U"  ||w--||    ",
-    };
-    const std::vector<std::u32string> cerdo1 = {
-        U"    _____  ",
-        U"^..^     \\9",
-        U"(oo)_____/ ",
-        U"   WW  WW"};
-    const std::vector<std::u32string> gallina1 = {
-        U"   \\\\  ",
-        U"   (o>  ",
-        U"\\\\_//)  ",
-        U" \\_/_)  ",
-        U"  _|_  "};
-    const std::vector<std::u32string> tractor1 = {
-        U"  ____",
-        U" /|_||\\`.__",
-        U"(   _    _ _\\",
-        U"=`-(_)--(_)-'"};
-    const std::vector<std::u32string> arbol1 = {
-        U"  &&&",
-        U" &&&&&",
-        U"   |"};
+    const std::vector<std::u32string> sol1 = {
+        U"    |    ",
+        U" \\ | /  ",
+        U"  \\*/   ",
+        U"--**O**--",
+        U"   /*\\   ",
+        U"  / | \\  ",
+        U"    |   "};
+      
+    const std::vector<std::u32string> nube1 = {
+        U"    __   _   ",
+        U"  _(  )_( )_ ",
+        U" (_   _    _)",
+        U"   (_) (__)  "};
+ 
+    const std::vector<std::u32string> flor1 = {
+        U"   _  ",
+        U".\\ /.",
+        U"< ~O~ >",
+        U" '/_\\' ",
+        U"\\ | / ",
+        U" \\|/ "};
 
-    const std::vector<std::u32string> perro = {
-        U"     __  ",
-        U"(___()'`;",
-        U"/,    /` ",
-        U"\\\\\"--\\\\  "};
+    const std::vector<std::u32string> venado1 = {
+        U"         { }    ",
+        U"         {^^,   ",
+        U"         (   `-;",
+        U"    _     `;;~~ ",
+        U"   /(______);   ",
+        U"  (         (   ",
+        U"   |:------( )  ",
+        U" _//         \\ ",
+        U"/ /          vv "};
+
+    const std::vector<std::u32string> cactus1 = {
+        U"  |  ",
+        U"(_|_)",
+        U"  |  "}; 
+
 
     GestorDibujos gestor;
-    gestor.Agregar(Dibujo(2, 1, granja1, ftxui::Color::White));     // granja blanca
-    gestor.Agregar(Dibujo(15, 2, arbol1, ftxui::Color::Green));     // árbol verde
-    gestor.Agregar(Dibujo(5, 4, arbol1, ftxui::Color::Green));      // árbol verde
-    gestor.Agregar(Dibujo(25, 3, arbol1, ftxui::Color::Green));     // árbol verde
-    gestor.Agregar(Dibujo(40, 5, arbol1, ftxui::Color::Green));     // árbol verde
-    gestor.Agregar(Dibujo(8, 10, vaca1, ftxui::Color::GrayLight));  // vaca gris
-    gestor.Agregar(Dibujo(20, 11, vaca1, ftxui::Color::GrayLight)); // vaca gris
-    gestor.Agregar(Dibujo(5, 15, cerdo1, ftxui::Color::Magenta));   // cerdo rosa
-    gestor.Agregar(Dibujo(25, 13, gallina1, ftxui::Color::Yellow)); // gallina amarilla
-    gestor.Agregar(Dibujo(30, 8, tractor1, ftxui::Color::Red));     // tractor rojo
-    gestor.Agregar(Dibujo(10, 6, perro, ftxui::Color::White));      // perro blanco
+    gestor.Agregar(Dibujo(2, 1, sol1, ftxui::Color::Yellow2));     // sol amarillo
+    gestor.Agregar(Dibujo(15, 2, nube1, ftxui::Color::White));     // nube blanca
+    gestor.Agregar(Dibujo(40, 4, nube1, ftxui::Color::White));     // nube blanca
+    gestor.Agregar(Dibujo(60, 4, nube1, ftxui::Color::White));     // nube blanca
+    
+    gestor.Agregar(Dibujo(12, 9, venado1, ftxui::Color::Red));     // venado rojo
 
-    // Crear corral más grande y vacas separadas dentro
-    Corral corral(50, 15, 30, 18, ftxui::Color::White);
-    corral.AgregarAnimal(Dibujo(67, 16, vaca1, ftxui::Color::GrayLight));
-    corral.AgregarAnimal(Dibujo(60, 19, vaca1, ftxui::Color::GrayLight));
-    corral.AgregarAnimal(Dibujo(53, 17, vaca1, ftxui::Color::GrayLight));
+    gestor.Agregar(Dibujo(70, 10, cactus1, ftxui::Color::Green));      // cactus verde
+    gestor.Agregar(Dibujo(60, 13, flor1, ftxui::Color::Magenta2));      // flor rosa
+    gestor.Agregar(Dibujo(50, 11, cactus1, ftxui::Color::Green));      // cactus verde
+    gestor.Agregar(Dibujo(35, 13, flor1, ftxui::Color::Magenta2));      // flor rosa
+    gestor.Agregar(Dibujo(20, 10, cactus1, ftxui::Color::Green));      // cactus verde
+    gestor.Agregar(Dibujo(8, 13, flor1, ftxui::Color::Magenta2));      // flor rosa
+ 
+  
+
 
     int frames = 120;
     for (int frame = 0; frame < frames; ++frame)
@@ -81,13 +80,21 @@ int main()
         screen.Clear();
 
         // Animación: mover vacas y tractor (índices actualizados)
-        gestor.dibujos[5].x = 8 + (frame % 10);  // vaca1
-        gestor.dibujos[6].x = 20 - (frame % 10); // vaca2
-        gestor.dibujos[9].x = 30 + (frame % 15); // tractor hacia la derecha
-        gestor.dibujos[7].x = 5 + (frame % 12);  // cerdito se mueve horizontalmente
+        gestor.dibujos[0].x = 2 + (frame % 2);  // sol1
 
+        gestor.dibujos[1].x = 15 + (frame % 8); // nube
+        gestor.dibujos[2].x = 40 + (frame % 8); // nube
+        gestor.dibujos[3].x = 60 + (frame % 8); // nube
+
+        gestor.dibujos[4].x = 12 + (frame % 12); // venado hacia la derecha
+
+        gestor.dibujos[5].x = 70 - (frame % 10); // flora hacia la izquierda
+        gestor.dibujos[6].x = 60 - (frame % 10); // flora hacia la izquierda
+        gestor.dibujos[7].x = 50 - (frame % 10); // flora hacia la izquierda
+        gestor.dibujos[8].x = 35 - (frame % 10); // flora hacia la izquierda
+        gestor.dibujos[9].x = 20 - (frame % 10); // flora hacia la izquierda
+        gestor.dibujos[10].x = 8 - (frame % 10); // flora hacia la izquierda
         gestor.DibujarTodos(screen);
-        corral.Dibujar(screen);
 
         // Imprime la pantalla y resetea el cursor
         std::cout << screen.ToString();
